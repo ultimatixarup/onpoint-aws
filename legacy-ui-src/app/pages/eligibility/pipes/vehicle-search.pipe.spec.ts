7c4026442df6edf8e31325e5fe4b6ece5d2467f9
Arup Banerjee <arup.banerjee@smallboard.com>
2026-02-11 16:19:32 -0600

    Remove legacy-ui-src

--- legacy-ui-src/app/pages/eligibility/pipes/vehicle-search.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { VehicleSearchPipe } from './vehicle-search.pipe';
-
-describe('VehicleSearchPipe', () => {
-  it('create an instance', () => {
-    const pipe = new VehicleSearchPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- legacy-ui-src/assets/js/devtools-hide.js
+++
@@ -1,7 +0,0 @@
- {
-  // Add the class to hide specific elements
-  document.addEventListener('DOMContentLoaded', () => {
-    const elements = document.querySelectorAll('.devtools-hidden');
-    elements.forEach(el => el.classList.add('devtools-hidden'));
-  });
-}
